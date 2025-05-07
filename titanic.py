12d70c79302860ba575a96fdbf7f091d8ad908b1
Tanushri <tanushrinarikimilli04@gmail.com>
2025-05-07 22:43:03 +0530

    Data Cleaning of Dataset

--- titanic.py
+++ titanic.py
@@ -8,3 +8,19 @@ print("Rows and Columns:", df.shape)
 print("Column Data Types:\n", df.dtypes)
 print("Null values:\n", df.isnull().sum())
 print("First few rows:\n", df.head())
+
+#data cleaning
+print("Missing values before cleaning:")
+print(df.isnull().sum())
+
+df.drop(['PassengerId', 'Name', 'Ticket', 'Cabin'], axis=1, inplace=True)
+df['Age'].fillna(df['Age'].median(), inplace=True)
+df['Embarked'].fillna(df['Embarked'].mode()[0], inplace=True)
+
+print("Duplicate rows:", df.duplicated().sum())
+df.drop_duplicates(inplace=True)
+
+print("Missing values after cleaning:")
+print(df.isnull().sum())
+
+print("Data cleaned ✅")
