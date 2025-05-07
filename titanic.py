e58d54c688414f0b5e8e615791c520103b37dc07
Tanushri <tanushrinarikimilli04@gmail.com>
2025-05-07 15:15:27 +0530

    WIP: Save local changes before merging main

--- titanic.py
+++ titanic.py
@@ -3,4 +3,8 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-df=pd.read_csv('train.csv')
+df=pd.read_csv('titanic dataset/train.csv')
+print("Rows and Columns:", df.shape)
+print("Column Data Types:\n", df.dtypes)
+print("Null values:\n", df.isnull().sum())
+print("First few rows:\n", df.head())
