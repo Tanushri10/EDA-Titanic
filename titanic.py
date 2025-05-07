9b65baadd394dc76ef3436ea06751897d108dedf
Tanushri <tanushrinarikimilli04@gmail.com>
2025-05-07 15:07:00 +0530

    Added initial data inspection (rows, columns, dtypes)

--- titanic.py
+++ titanic.py
@@ -2,18 +2,10 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-import pandas as pd
 
-df = pd.read_csv('titanic dataset/train.csv')
+df=pd.read_csv('titanic dataset/train.csv')
 
 print("Rows and Columns:", df.shape)
 print("Column Data Types:\n", df.dtypes)
 print("Null values:\n", df.isnull().sum())
 print("First few rows:\n", df.head())
-
-
-
-
-
-
-
