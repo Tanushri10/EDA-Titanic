acc9c4db6decadaca5f15f32ee35729e38bc2a62
Tanushri <tanushrinarikimilli04@gmail.com>
2025-05-07 22:54:16 +0530

    Editing some warning caused in terminal

--- titanic.py
+++ titanic.py
@@ -14,13 +14,33 @@ print("Missing values before cleaning:")
 print(df.isnull().sum())
 
 df.drop(['PassengerId', 'Name', 'Ticket', 'Cabin'], axis=1, inplace=True)
-df['Age'].fillna(df['Age'].median(), inplace=True)
-df['Embarked'].fillna(df['Embarked'].mode()[0], inplace=True)
+df['Age'] = df['Age'].fillna(df['Age'].median())
+df['Embarked'] = df['Embarked'].fillna(df['Embarked'].mode()[0])
 
-print("Duplicate rows:", df.duplicated().sum())
+
 df.drop_duplicates(inplace=True)
 
 print("Missing values after cleaning:")
 print(df.isnull().sum())
 
 print("Data cleaned ✅")
+
+
+# 1. Countplot of the 'Survived' column
+plt.figure(figsize=(8, 5))
+sns.countplot(x='Survived', data=df, palette='Set2')
+plt.title('Survived vs Not Survived')
+plt.show()
+
+# 2. Age distribution of passengers
+plt.figure(figsize=(8, 5))
+sns.histplot(df['Age'], bins=30, kde=True, color='blue')
+plt.title('Age Distribution')
+plt.show()
+
+# 3. Sex vs Survived
+plt.figure(figsize=(8, 5))
+sns.countplot(x='Survived', hue='Sex', data=df, palette='Set1')
+plt.title('Survival Count by Sex')
+plt.show()
+
